ca5eb1dd4a3caa4db2dc581fa98ed6e60bb6238d
Student <example@example.com>
2019-04-09 13:21:20 -0700

    router link forum to boards

--- src/app/boards/boards.component.ts
+++ src/app/boards/boards.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DatabaseService } from '../database.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
-  styleUrls: ['./boards.component.scss']
+  styleUrls: ['./boards.component.scss'],
+  providers: [DatabaseService]
 })
 export class BoardsComponent implements OnInit {
+  boards: FirebaseListObservable<any[]>;
+  threads: FirebaseListObservable<any[]>;
+  replies: FirebaseListObservable<any[]>;
 
-  constructor() { }
+
+  constructor(private database: DatabaseService) { }
 
   ngOnInit() {
+    this.boards = this.database.getBoards();
+    // this.threads = this.database.getThreads('Music');
+  }
+  loadBoard(board) {
+    this.threads = board.threads;
   }
 
 }
