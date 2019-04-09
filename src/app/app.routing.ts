4c5feb8b62fec403b3ef3f1f40a294b3c6332a48
Student <example@example.com>
2019-04-09 13:18:53 -0700

    add boards component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ForumComponent } from './forum/forum.component';
 import { LandingComponent } from './landing/landing.component';
+import { BoardsComponent } from './boards/boards.component';
 
 const appRoutes: Routes = [
   {
@@ -11,6 +12,10 @@ const appRoutes: Routes = [
   {
     path: 'forum',
     component: ForumComponent
+  },
+  {
+    path: 'boards',
+    component: BoardsComponent
   }
 ];
 
