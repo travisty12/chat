c95a138d7245d8e9728bc5af753c8dc07055846b
Student <example@example.com>
2019-04-09 11:15:39 -0700

    add new post/thread functionality

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DatabaseService } from '../database.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { Reply } from '../reply.model';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-forum',
@@ -13,6 +15,8 @@ export class ForumComponent implements OnInit {
   @Input() activeBoard;
   @Input() threadShown;
   @Input() threads;
+  a;
+  postThread = false;
 
   constructor(private databaseService: DatabaseService) { }
 
@@ -23,12 +27,33 @@ export class ForumComponent implements OnInit {
     this.replies = thread.replies;
   }
 
-  newThread() {
-
+  newThread(text: string, name: string, title: string, board) {
+    const image = "image goes here";
+    const post = new Post(title, name, text, image);
+    const boardName = board['$key'];
+    const i = this.threads.length;
+    this.databaseService.addThread(post, boardName, i);
+    return;
   }
 
-  submitPost(text: string, name: string) {
-    databaseService.addReply()
+  submitPost(text: string, name: string, thread, board) {
+    const image = "image goes here";
+    this.a = this.replies;
+    let i;
+    if (this.a) {
+      i = this.a.length;
+    } else {
+      i = 0;
+    }
+    const reply = new Reply(text, name, image);
+    const boardName = board['$key'];
+    for (let j = 0; j < this.threads.length; j++) {
+      if (this.threads[j] == thread) {
+        const threadIndex = j;
+        this.databaseService.addReply(reply, threadIndex, boardName, i);
+      }
+    }
+    return;
   }
 
 
