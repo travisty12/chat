5532a2b92cb319999917055f0af2ede76ef75d88
Student <example@example.com>
2019-04-10 12:10:03 -0700

    add customer colors on navbar depending on what page the user goes to

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,26 +11,32 @@ export class NavbarComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.style = document.getElementsByClassName('all')
+    console.log(this.style[0]);
   }
+  style;
   extendMenu;
 
   menu(){
     if (this.extendMenu){
       return "extended";
     }else {
-      return 'navbar';
+      return "navbar";
     }
   }
   toChat(){
     this.router.navigate(['chat']);
     this.extendMenu = false;
+    this.style[0].classList = 'all purple';
   }
   toForum(){
     this.router.navigate(['boards']);
     this.extendMenu = false;
+    this.style[0].classList = 'all green';
   }
   toNews(){
     this.router.navigate(['news']);
     this.extendMenu = false;
+    this.style[0].classList = 'all orange'
   }
 }
