ab93fb528fdea738531e2ca033964c6ebd8102bb
Student <example@example.com>
2019-04-10 10:17:04 -0700

    add timestamp and sanitize inputs

--- src/app/message.model.ts
+++ src/app/message.model.ts
@@ -1,4 +1,5 @@
 export class Message {
   public timestamp = Date.now();
+  public time = new Date(Date.now()).toLocaleString();
   constructor(public text: string, public name: string) {};
 }
--- src/app/post.model.ts
+++ src/app/post.model.ts
@@ -1,5 +1,6 @@
 export class Post {
   public timestamp = Date.now();
+  public time = new Date(Date.now()).toLocaleString();
   public recentTimestamp = Date.now();
   constructor(public title: string, public username: string, public comment: string, public image: string) {};
 }
