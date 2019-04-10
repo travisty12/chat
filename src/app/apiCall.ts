2d8bde25a158f62bb8c04e53a1db14d94777a48f
Student <example@example.com>
2019-04-10 14:10:33 -0700

    fix news api key

--- src/app/apiCall.ts
+++ src/app/apiCall.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-
+import { masterFirebaseConfig } from './api-keys';
 
 @Injectable()
 export class ApiCall {
   constructor(public http: Http) { }
   callApi() {
-    return this.http.get('https://newsapi.org/v2/everything?q=cryptocurrency&from=2019-03-10&sortBy=publishedAt&apiKey=402a12f57a0545618f8dbe1d5ce49af5');
+    return this.http.get(`https://newsapi.org/v2/everything?q=cryptocurrency&from=2019-03-10&sortBy=publishedAt&apiKey=${masterFirebaseConfig.newsApi}`);
   }
 }
