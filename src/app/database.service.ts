e7e93ad4d5fa13a309e6cae3819555bc0dcc59b0
Student <example@example.com>
2019-04-10 16:27:59 -0700

    will delete older chats

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -9,6 +9,7 @@ export class DatabaseService {
   threads: FirebaseListObservable<any[]>;
   chat: FirebaseListObservable<any[]>;
   a;
+  chatLength;
   constructor(private database: AngularFireDatabase) {
     this.boards = database.list('boards');
     this.chat = database.list('chat/anonymous');
@@ -118,27 +119,54 @@ export class DatabaseService {
     chatEntryInFirebase.remove();
   }
 
-  deleteChat(id) {
+  removeChat(id) {
     id.remove();
   }
 
-
   autoDeleteChat(info) {
-    console.log(info);
     let currentTime = Date.now();
-    let hour = currentTime - 3600000;
-    console.log(currentTime);
+    let hour = currentTime - 36000;
     let length = info.length;
-    console.log(length);
+    let array = [];
     for(let i = 0; i < length; i++) {
+      let selected = [];
       let time = info[i].timestamp;
       if(time < hour) {
-        let id = this.getChatById(info[i].$key);
-        console.log("IS Less than");
-        console.log(id);
-        console.log(info[i].$key);
-        this.deleteChat(id);
+        array.push(i);
       }
+    }
+    for(let e = 0; e < array.length; e++) {
+      this.reAssign(info);
+      let id = this.getChatById(info[e].$key);
+      this.removeChat(id);
+
+    }
+  }
+
+  reAssign(info) {
+    let length = info.length;
+    let last = this.getChatById(info[length - 1].$key);
+    for(let i = 0; i < length; i++) {
+      let id = this.getChatById(info[i].$key);
+      let a = i + 1;
+      let currentInfo = info[a];
+      let nextname;
+      let nextText;
+      let nextTime;
+      let nextTimeStamp;
+      if(currentInfo) {
+        nextname = currentInfo.name;
+        nextText = currentInfo.text
+        nextTime = currentInfo.time;
+        nextTimeStamp = currentInfo.timestamp;
+
+      }
+      firebase.database().ref(`chat/anonymous/${i}`).set({
+        'text': `${nextText}`,
+        'name': `${nextname}`,
+        'time': `${nextTime}`,
+        'timestamp': `${nextTimeStamp}`
+      });
     }
   }
 }
